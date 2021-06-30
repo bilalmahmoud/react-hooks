876ad83ccc0e5f87488b7d6c2c327693203f1bfe
bil mah <bilal_m_12_21@hotmail.com>
2021-06-30 14:28:38 +0300

    export custom hook

--- src/hooks/useSortedArray.js
+++ src/hooks/useSortedArray.js
@@ -1,8 +1,10 @@
+/* Custom hook that sort array desc or asc */
 const useSortedList = (array, order) => {
   array =
     order === "asc"
       ? array.sort((a, b) => (a > b ? -1 : 1))
-      : array.sort((a, (b) => (a > b ? 1 : 1)));
+      : array.sort((a, b) => (a > b ? 1 : 1));
 
   return { array };
 };
+export default useSortedList;
